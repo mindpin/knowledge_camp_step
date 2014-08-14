f6236fc7582c2cd929f18c2f7cbd92219d3901d3
Kaid <kaid@kaid.me>
2014-08-14 16:07:30 +0800

    added Step branch selection

--- lib/knowledge_camp/step.rb
+++ lib/knowledge_camp/step.rb
@@ -5,15 +5,47 @@ require "knowledge_camp/note"
 
 module KnowledgeCamp
   class Step
+    CONTINUE_TYPES = [:end, :id, :select].freeze
+
     include Mongoid::Document
     include Mongoid::Timestamps
     include SortChar::Owner
 
-    field :title, :type => String
-    field :desc,  :type => String
+    field :title,         :type => String
+    field :desc,          :type => String
+    field :continue_type, :type => Symbol
+    field :continue
 
     has_many   :notes,   :class_name  => Note.name
     belongs_to :stepped, :polymorphic => true
+
+    validate :validate_continue_and_continue_type
+
+    def validate_continue_and_continue_type
+      return if continue_type.nil?
+
+      if !CONTINUE_TYPES.include?(self.continue_type.to_sym)
+        return errors.add(:continue_type, "Invalid continue_type value!")
+      end
+
+      message = case self.continue_type
+                when :end
+                  cond = self.continue == :end
+                  cond ? nil : "Invalid `:end' continue value!"
+                when :id
+                  cond = self.continue.is_a?(Hash) && self.continue[:id]
+                  cond ? nil : "Invalid `:id' continue value!"
+                when :select
+                  cond = self.continue.is_a?(Hash) && self.continue[:select]
+                  cond ? nil : "Invalid `:select' continue value!"
+                end
+
+      errors.add(:continue, message) if message
+    end
+
+    def attrs_simple
+      {:id => self.id.to_s, :title => self.title}
+    end
 
     def attrs
       {
--- spec/knowledge_camp_step_spec.rb
+++ spec/knowledge_camp_step_spec.rb
@@ -4,14 +4,82 @@ module KnowledgeCamp
   describe Step do
     let(:model)   {Stepped.create}
     let(:creator) {User.create}
-    let(:step)    {model.steps.create}
-    let(:note)    {step.notes.create(:creator => creator, :content => "content", :kind => "whatever")}
+    let(:step1)   {model.steps.create}
+    let(:step2)   {model.steps.create}
+    let(:step3)   {model.steps.create}
+    let(:note)    {step1.notes.create(:creator => creator, :content => "content", :kind => "whatever")}
+
+    let(:end_cont)    {:end}
+    let(:id_cont1)    {step2.attrs_simple}
+    let(:id_cont2)    {step3.attrs_simple}
+    let(:select_cont) {{
+      :select => {
+        :question => "选择下一个Step.",
+        :options  => [id_cont1, id_cont2]
+      }
+    }}
 
     before {note}
 
-    it {expect(step.stepped).to eq model}
+    it {expect(step1.stepped).to eq model}
     it {expect(note.creator).to eq creator}
     it {expect(model.notes).to include note}
     it {expect(creator.notes).to include note}
+
+    context "when set wrong continue_type & continue value" do
+      context "when wrong continue_type value" do
+        before {step1.continue_type = :haha}
+        
+        it {expect(step1).to be_invalid}
+      end
+
+      context "when wrong end value" do
+        before do
+          step1.continue_type = :end
+          step1.continue = id_cont1
+        end
+
+        it {expect(step1).to be_invalid}
+
+        it {
+          step1.continue = end_cont
+          expect(step1).to be_valid
+          step1.save
+          expect(step1.continue).to eq end_cont
+        }
+      end
+
+      context "when wrong id value" do
+        before do
+          step1.continue_type = :id
+          step1.continue = select_cont
+        end
+
+        it {expect(step1).to be_invalid}
+
+        it {
+          step1.continue = id_cont1
+          expect(step1).to be_valid
+          step1.save
+          expect(step1.continue).to eq id_cont1
+        }
+      end
+
+      context "when wrong select value" do
+        before do
+          step1.continue_type = :select
+          step1.continue = end_cont
+        end
+
+        it {expect(step1).to be_invalid}
+
+        it {
+          step1.continue = select_cont
+          expect(step1).to be_valid
+          step1.save
+          expect(step1.continue).to eq select_cont
+        }
+      end
+    end
   end
 end
